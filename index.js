12002ccc8b4249e8db299afee71e84aad32a9af2
sreevaniquin <sreevani@quintessential.design>
2022-08-04 13:18:46 +0530

    commit

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const mongoose = require("mongoose");
 app.use(cors()); //allow cors to all request
 const db =
   "mongodb+srv://Optimistic31:Optimistic31@cluster0.5rz1n.mongodb.net/?retryWrites=true&w=majority";
-mongoose
-  .connect(db)
-  .then((res, rej) => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const m=mongoose.connect(db)
+const a=async()=>{
+    const s=await m
+    console.log(s);
+}
+a()
+// console.log(m);
+//   .then((res, rej) => {
+//     console.log("connected");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 //console.log(m);
 app
   .get("/", (req, res) => {
